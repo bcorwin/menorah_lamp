79b4123634dde578132cc92dc03ad1a18c1033d7
Ben (ML) <bscorwin@gmail.com>
2024-12-19 14:05:46 -0500

    use one endpoint

--- webapp/app.py
+++ webapp/app.py
@@ -4,29 +4,34 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/light", methods=["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
 def light():
-  if request.method == "GET":
-    return render_template("light.html", today=date.today())
+  if request.method == "POST":
+    d = request.form
+    if d.get("light") == "":
+      cmd = ["sudo", "../light_menorah.sh"]
+      cmd.extend(["--date", d["run_as_date"]])
+      cmd.extend(["--sleep", d["run_length"]])
 
-  d = request.form
-  if d.get("light") == "":
-    cmd = ["sudo", "../light_menorah.sh"]
-    cmd.extend(["--date", d["run_as_date"]])
-    cmd.extend(["--sleep", d["run_length"]])
+      palette = d["palette"]
+      if palette != "None":
+        cmd.extend(["--palette", palette])
 
-    palette = d["palette"]
-    if palette != "None":
-      cmd.extend(["--palette", palette])
-
-    pattern = d["pattern"]
-    if pattern != "None":
-      cmd.extend(["--pattern", pattern])  
-    output = "Lighting the menorah."
+      pattern = d["pattern"]
+      if pattern != "None":
+        cmd.extend(["--pattern", pattern])  
+      message = "Lighting the menorah."
+    else:
+      cmd = ["sudo", "../extinguish_menorah.sh"]
+      message = "Putting out the menorah."
+    run(cmd)
   else:
-    cmd = ["sudo", "../extinguish_menorah.sh"]
-    output = "Putting out the menorah."
- 
-  run(cmd)
+    message = None
+  
+  output = render_template(
+    "light.html",
+    today=date.today(),
+    message = message
+  )
   return output
 
