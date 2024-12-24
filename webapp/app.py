9b305388596a432294fd81bed227455f6662c566
Ben (ML) <bscorwin@gmail.com>
2024-12-24 11:53:13 -0500

    redirect to viewing page

--- webapp/app.py
+++ webapp/app.py
@@ -1,37 +1,42 @@
 from datetime import date
-from subprocess import run
-from flask import Flask, render_template, request
+import subprocess
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST", "GET"])
-def light():
-  if request.method == "POST":
-    d = request.form
-    if d.get("light") == "":
-      cmd = ["sudo", "../light_menorah.sh"]
-      cmd.extend(["--date", d["run_as_date"]])
-      cmd.extend(["--sleep", d["run_length"]])
-
-      palette = d["palette"]
-      if palette != "None":
-        cmd.extend(["--palette", palette])
-
-      pattern = d["pattern"]
-      if pattern != "None":
-        cmd.extend(["--pattern", pattern])  
-      message = "Lighting the menorah."
-    else:
-      cmd = ["sudo", "../extinguish_menorah.sh"]
-      message = "Putting out the menorah."
-    run(cmd)
-  else:
-    message = None
-  
+@app.route("/", methods=["GET"])
+def config():
   output = render_template(
     "light.html",
-    today=date.today(),
-    message = message
-  )
+    today=date.today()
+ )
   return output
 
+@app.route("/set_state", methods=["POST"])
+def set_state():
+  d = request.form
+  if d.get("light") == "":
+    cmd = ["sudo", "../light_menorah.sh"]
+    cmd.extend(["--date", d["run_as_date"]])
+    cmd.extend(["--sleep", d["run_length"]])
+
+    palette = d["palette"]
+    if palette != "None":
+      cmd.extend(["--palette", palette])
+
+    pattern = d["pattern"]
+    if pattern != "None":
+      cmd.extend(["--pattern", pattern])  
+    message = "Lighting the menorah."
+  else:
+    cmd = ["sudo", "../extinguish_menorah.sh"]
+    message = "Putting out the menorah."
+  #results = run(cmd)
+  output = subprocess.check_output(cmd, text=True, timeout=10)
+
+  print(output)
+  return redirect(url_for("view"))
+
+@app.route("/view", methods=["GET"])
+def view():
+  return "Viewing"
