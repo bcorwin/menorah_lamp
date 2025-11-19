cca9b85c3c3210f41b12a3d58efeb4ee606f58ec
Ben (ML) <bscorwin@gmail.com>
2025-11-19 13:00:21 -0500

    move patterns to own file

--- menorah.py
+++ menorah.py
@@ -9,8 +9,9 @@ import colr
 import click
 import signal
 import palettes as p
+import patterns
 import holiday_dates as hd
-from random import randint, choice, choices
+from random import choice
 from datetime import date, timedelta, datetime
 
 
@@ -144,78 +145,9 @@ class Menorah:
     def light(self, lights, color=(255, 255, 255), fade=0):
         self._lights_on(lights, [color], fade=fade)
 
-    # Fun patterns
-    def fan_out(self, colors=(255, 255, 255), delay=0.25, fade=0, keep_on=True):
-        if not isinstance(colors, list):
-            colors = [colors]
-
-        for i in range(5):
-            color = colors[i % len(colors)]
-            lights = [4+i, 4-i]
-            self._lights_on(lights, [color], fade=fade)
-            time.sleep(delay)
-            if not keep_on:
-                self._lights_off(lights, fade=fade)
-        if keep_on:
-            self.off(fade=fade)
-
-    def color_chase(self, lights, color=(255, 255, 255), delay=0.25, fade=0):
-        for light in lights:
-            self._lights_on([light], [color], fade=fade)
-            time.sleep(delay)
-
-    def snake(self, lights, color=(255, 255, 255), delay=0.25, fade=0.01, snake_size=3):
-        # TODO: Could this replace color chase?
-        # TOOD: Add flag to loop around instead of stopping at end then restarting
-        off_color = (0, 0, 0)
-        num_lights = len(lights)
-        if snake_size >= num_lights:
-            raise ValueError("snake_size must be less than num_lights")
-
-        for snake_tail in range(num_lights):
-            # Index of the head of the snake
-            snake_head = snake_tail + snake_size - 1
-
-            # Build the snake to start
-            if snake_tail == 0:
-              self._lights_on(lights[snake_tail], color, fade=fade)
-              time.sleep(delay)
-              for j in range(1, snake_size):
-                # print(f"{snake_tail}-P3")
-                self._lights_on(lights[j], color, fade=fade)
-                time.sleep(delay)
-
-            # Remove the tail and move the head forward
-            if snake_head < num_lights - 1:             
-              self._lights_on(
-                  [lights[snake_tail], lights[snake_head + 1]],
-                  [off_color, color],
-                  fade = fade
-              )
-              time.sleep(delay)
-            elif snake_head == (num_lights - 1):
-              # Remove remaining snake at end
-              for j in range(snake_tail, snake_head + 1):
-                  # print(f"{snake_tail}-P2-{j}")
-                  self._lights_off(lights[j], fade=fade)
-                  time.sleep(delay)  # Don't do this for the last one?
-    
-    def random(self, lights, colors, max_num=None, fade=0):
-        if max_num is None:
-            max_num = len(lights)
-        assert max_num <= len(lights), "max_num must be less than len(lights)"
-        
-        num = randint(1, max_num)
-        new_lights = [choice(lights) for _ in range(num)]
-        new_colors = [choice(colors) for _ in range(num)] 
-                
-        self._lights_on(new_lights, new_colors, fade=fade)
-
-    def blink(self, lights, delay=0.1, fade=0):
-        colors = self._get_colors(lights)
-        self._lights_off(lights, fade=fade)
-        time.sleep(delay)
-        self._lights_on(lights, colors, fade=fade)
+    def run_pattern(self, pattern, **kwargs):
+        f = getattr(patterns, pattern)
+        f(self, **kwargs)
 
     def print(self, message, log=True, **kwargs):
       if log:
@@ -226,37 +158,50 @@ class Menorah:
         print(message, **kwargs)
 
 @click.command()
-@click.option("--date",
+@click.option(
+    "--date",
     "date_to_run",
     default=str(date.today()),
     type=click.DateTime(formats=["%Y-%m-%d"]),
-    help="Date to run as.")
-@click.option("--sleep",
+    help="Date to run as."
+)
+@click.option(
+    "--sleep",
     default=4.5,
     type=click.FLOAT,
-    help="How long to run for (in hours).")
-@click.option("--palette", "-p",
+    help="How long to run for (in hours)."
+)
+@click.option(
+    "--colors",
+    "-c",
+    "palette",
     default=None,
-    help="Palette name. See palettes.py for options.")
-@click.option("--keep-on/--no-keep-on",
+    help="Palette name / color-set. See palettes.py for options."
+)
+@click.option(
+    "--keep-on/--no-keep-on",
     default=None,
-    help="Keep on for the fan_out pattern.")
-@click.option("--pattern",
+    help="Keep on for the fan_out pattern."
+)
+@click.option(
+    "--pattern",
     default=None,
     help="Pattern to use."
 )
 def main(date_to_run=None, sleep=None, palette=None, keep_on=None, pattern=None):
+    # TODO: Accept any possible keyword args like fade, delay, etc.
     stop_time = time.time() + 60 * 60 * sleep
     try:
         menorah = Menorah()
-        patterns = ["fan_out", "color_chase", "random", "snake", "growing_snake"]
+        pattern_names = [f for f in dir(patterns) if "__" not in f]
 
         date_to_run = date_to_run.date()
         menorah.print(f"Date: {date_to_run}")
+
         night = hd.chanukah_nights.get(date_to_run)
         if night is not None:
             lights = menorah.get_lights(night)
-            patterns.remove("fan_out")
+            pattern_names.remove("fan_out")
             menorah.print(f"Night: {night}")
         else:
             lights = menorah.get_lights(8)
@@ -267,7 +212,7 @@ def main(date_to_run=None, sleep=None, palette=None, keep_on=None, pattern=None)
                 raise ValueError("Invalid palette")
             else:
                 palette = getattr(p, palette)
-                assert isinstance(palette, p.Colors), "Palette is not a palette"
+                assert isinstance(palette, p.Colors), f"{palette} is not a palette"
         else:
             if date_to_run in hd.christmas_dates:
                 palette = p.christmas
@@ -277,41 +222,24 @@ def main(date_to_run=None, sleep=None, palette=None, keep_on=None, pattern=None)
                palette = p.random()
         menorah.print(f"Palette: {palette}")
 
+        # TODO: Move things like this to their patterns function (above too?)
         if keep_on is None:
             keep_on = choice([True, False])
 
         if pattern is None:
-            pattern = choice(patterns)
+            pattern = choice(pattern_names)
         else:
-            assert pattern in patterns, f"Invalid pattern name: {pattern}"
+            assert pattern in pattern_names, f"Invalid pattern name: {pattern}"
         menorah.print(f"Pattern: {pattern}")
 
         while time.time() < stop_time:
-            if pattern == "fan_out":
-                menorah.fan_out(colors=palette.get_next(5), fade=.25, keep_on=keep_on)
-            elif pattern == "color_chase":
-                menorah.color_chase(lights, color=palette.get_next(), fade=1)
-            elif pattern == "phi":
-                menorah.phi(lights, color=palette.get_next())
-            elif pattern == "blink":
-                # TODO: Make this better before using it. It's boring.
-                menorah.light(lights, color=palette.get_next(), fade=1)
-                for _ in range(10):
-                    blink_lights = choices(lights,
-                                           k=randint(0, len(lights) // 3))
-                    menorah.blink(blink_lights, fade=0.1)
-                    time.sleep(0.1)
-            elif pattern == "random":
-                menorah.light(lights, color=palette.get_next(), fade=1)
-                menorah.random(lights, colors=palette.get_all(), fade=1)
-            elif pattern == "snake":
-                num_lights = len(lights)
-                snake_size = 3 if num_lights > 3 else num_lights - 1
-                menorah.snake(lights, color=palette.get_next(), snake_size = snake_size)
-            elif pattern == "growing_snake":
-                for snake_size in range(1, len(lights)):
-                    menorah.snake(lights, color=palette.get_next(), snake_size=snake_size)
-            # TODO: Add Magni-phi effect (or is this just color chase?)
+            menorah.run_pattern(
+                pattern=pattern,
+                lights=lights,
+                palette=palette,
+                keep_on=keep_on,
+                snake_size = 3 if len(lights) > 3 else len(lights) - 1,
+            )
 
     finally:
         menorah.off()
---
+++ patterns.py
@@ -0,0 +1,99 @@
+import time
+from random import randint, choice
+
+
+def fan_out(lamp, lights, palette, **kwargs):
+    delay = kwargs.get("delay", 0.25)
+    fade = kwargs.get("fade", 0.25)
+    keep_on = kwargs.get("keep_on", True)
+
+    colors = palette.get_next(5)
+
+    if not isinstance(colors, list):
+        colors = [colors]
+
+    for i in range(5):
+        color = colors[i % len(colors)]
+        lights = [4+i, 4-i]
+        lamp._lights_on(lights, [color], fade=fade)
+        time.sleep(delay)
+        if not keep_on:
+            lamp._lights_off(lights, fade=fade)
+    if keep_on:
+        lamp.off(fade=fade)
+
+def random(lamp, lights, palette, **kwargs):
+    max_num = kwargs.get("max_num", len(lights))
+    fade = kwargs.get("fade", 1)
+
+    # Turn on all the lights to start
+    lamp.light(lights, color=palette.get_next(), fade=fade)
+    colors = palette.get_all()
+
+    if max_num > len(lights):
+        raise ValueError("max_num must be less than len(lights)")
+
+    # Now change them randomly
+    num = randint(1, max_num)
+    new_lights = [choice(lights) for _ in range(num)]
+    new_colors = [choice(colors) for _ in range(num)] 
+
+    lamp._lights_on(new_lights, new_colors, fade=fade)
+
+def color_chase(lamp, lights, palette, **kwargs):
+    delay = kwargs.get("delay", 0.25)
+    fade = kwargs.get("fade", 1)
+
+    color = palette.get_next()
+
+    for light in lights:
+        lamp._lights_on([light], [color], fade=fade)
+        time.sleep(delay)
+
+def snake(lamp, lights, palette, **kwargs):
+    # TODO: Could this replace color chase?
+    # TODO: Add flag to loop around instead of stopping at end then restarting
+    # TODO: Always keep the shamash on?
+    # TODO: Implement growing:
+    # elif pattern == "growing_snake":
+    #     for snake_size in range(1, len(lights)):
+    #         menorah.snake(lights, color=palette.get_next(), snake_size=snake_size)
+    delay = kwargs.get("delay", 0.25)
+    fade = kwargs.get("fade", 0.01)
+    snake_size = kwargs.get("snake_size", 3)
+    growing = kwargs.get("growing", False)
+
+    color = palette.get_next()
+
+    off_color = (0, 0, 0)
+    num_lights = len(lights)
+    if snake_size >= num_lights:
+        raise ValueError("snake_size must be less than num_lights")
+
+    for snake_tail in range(num_lights):
+        # Index of the head of the snake
+        snake_head = snake_tail + snake_size - 1
+
+        # Build the snake to start
+        if snake_tail == 0:
+            lamp._lights_on(lights[snake_tail], color, fade=fade)
+            time.sleep(delay)
+            for j in range(1, snake_size):
+                # print(f"{snake_tail}-P3")
+                lamp._lights_on(lights[j], color, fade=fade)
+                time.sleep(delay)
+
+        # Remove the tail and move the head forward
+        if snake_head < num_lights - 1:             
+            lamp._lights_on(
+                [lights[snake_tail], lights[snake_head + 1]],
+                [off_color, color],
+                fade = fade
+            )
+            time.sleep(delay)
+        elif snake_head == (num_lights - 1):
+            # Remove remaining snake at end
+            for j in range(snake_tail, snake_head + 1):
+                # print(f"{snake_tail}-P2-{j}")
+                lamp._lights_off(lights[j], fade=fade)
+                time.sleep(delay)  # Don't do this for the last one?
